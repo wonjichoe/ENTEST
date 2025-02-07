b815c22ea10f24fa8d8d8aa2828d7c6293ea402a
wonjichoe <wonjichoe@gmail.com>
2025-02-07 10:16:32 +0900

    Feat: Intro 슬라이더 이미지 추가

--- src/components/IntroCarousel.jsx
+++ src/components/IntroCarousel.jsx
@@ -2,8 +2,8 @@ import "react-responsive-carousel/lib/styles/carousel.min.css"
 import { Carousel } from "react-responsive-carousel"
 import { useState } from "react";
 import img1 from '@assets/images/main.jpg'
-import img2 from '@assets/images/main.jpg'
-import img3 from '@assets/images/main.jpg'
+import img2 from '@assets/images/main2.jpg'
+import img3 from '@assets/images/main3.jpg'
 import { IoIosArrowBack } from "react-icons/io"
 import { IoIosArrowForward } from "react-icons/io"
 
@@ -26,7 +26,7 @@ export function IntroCarousel() {
   }
 
   return (
-    <section className="relative h-[400px] sm:h-[600px] md:h-[746px] overflow-hidden">
+    <section className="relative h-[400px] sm:h-[500px] md:h-[700px] lg:h-[746px] overflow-hidden">
       <Carousel 
         selectedItem={currentSlide}
         onChange={setCurrentSlide}
@@ -41,8 +41,8 @@ export function IntroCarousel() {
           <div key={index} className="relative h-[400px] sm:h-[600px] md:h-[746px]">
             <img src={slide.image} alt={slide.caption} className="absolute top-0 left-0 w-full h-full object-cover" />
             <div className="absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center">
-              <div className="w-full max-w-[375px] sm:max-w-[810px] lg:max-w-[1440px] mx-auto text-left">
-                <p className="text-white text-2xl sm:text-4xl md:text-6xl md:leading-[1.4] font-bold mb-4 sm:mb-8 md:mb-14 max-w-full sm:max-w-[80%] md:max-w-[60%] break-keep">
+              <div className="w-full max-w-[280px] sm:max-w-[600px] md:max-w-[650px] lg:max-w-[1000px] 2xl:max-w-[1440px] mx-auto text-left">
+                <p className="text-white text-2xl sm:text-3xl md:text-5xl md:leading-[1.4] lg:text-6xl lg:leading-[1.4] font-bold mb-4 sm:mb-8 md:mb-14 max-w-full sm:max-w-[80%] md:max-w-[60%] break-keep">
                   {slide.caption}
                 </p>
                 <p className="text-gray-50 text-sm sm:text-base md:text-xl">
@@ -55,13 +55,13 @@ export function IntroCarousel() {
       </Carousel>
       <button
         onClick={prev}
-        className="absolute left-2 sm:left-4 top-1/2 transform -translate-y-1/2 text-white text-4xl sm:text-5xl md:text-6xl"
+        className="absolute left-1 md:left-4 top-1/2 transform -translate-y-1/2 text-white text-4xl sm:text-5xl md:text-6xl"
       >
         <IoIosArrowBack />
       </button>
       <button
         onClick={next}
-        className="absolute right-2 sm:right-4 top-1/2 transform -translate-y-1/2 text-white text-4xl sm:text-5xl md:text-6xl"
+        className="absolute right-1 md:right-4 top-1/2 transform -translate-y-1/2 text-white text-3xl sm:text-4xl md:text-5xl lg:text-6xl"
       >
         <IoIosArrowForward />
       </button>
